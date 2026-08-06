bffc93806c452d1e360b3a5b5bc5ccd6e5d90b1a
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-08-06 17:24:58 -0500

    feat: repositorio para rol con metodos para buscar por nombre

---
+++ backend/src/main/java/com/isai/backend/repositories/RolRepository.java
@@ -0,0 +1,12 @@
+package com.isai.backend.repositories;
+
+import com.isai.backend.domain.entities.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByNombre(String nombre);
+}
